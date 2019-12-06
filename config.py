0b08b802ca0b9a7964e52e17bd757f6405c5519a
limitimil <zlsh10340@gmail.com>
2019-12-07 01:29:05 +0800

    Initialization, with position information

---
+++ config.py
@@ -0,0 +1,33 @@
+cuts = {
+    'mine': {
+        'down': 500,
+        'left': 120,
+        'right': 145,
+        'up': 460,
+    },
+    'total': {
+        'down': 500,
+        'left': 310,
+        'right': 430,
+        'up': 460,
+    },
+    'days': {
+        'down': 270,
+        'left': 550,
+        'right': 590,
+        'up': 240,
+    },
+    'hours': {
+        'down': 270,
+        'left': 610,
+        'right': 650,
+        'up': 240,
+    },
+    'minutes': {
+        'down': 270,
+        'left': 670,
+        'right': 710,
+        'up': 240,
+    }
+
+}
